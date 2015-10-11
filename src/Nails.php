8f14f582c9bee1d62607f60d18376c4e8f644a10
Pablo de la Peña <p@shedcollective.org>
2015-10-11 17:35:02 +0100

    [WIP] Implementing the lookup method

--- src/Nails.php
+++ src/Nails.php
@@ -4,4 +4,22 @@ namespace Nails\GeoIp\Driver;
 
 class Nails implements \Nails\GeoIp\Interfaces\Driver
 {
+    /**
+     * @param string $sIp  The IP address to look up
+     * @return \Nails\GeoIp\Result\Ip
+     */
+    public function lookup($sIp)
+    {
+        $oResult = \Nails\Factory::factory('Ip', 'nailsapp/module-geo-ip');
+
+        $oResult->setIp($sIp);
+        $oResult->setHostname('The Hostname');
+        $oResult->setCity('The City');
+        $oResult->setRegion('The Region');
+        $oResult->setCountry('The Country');
+        $oResult->setLat('The Lat');
+        $oResult->setLng('The Lng');
+
+        return $oResult;
+    }
 }
